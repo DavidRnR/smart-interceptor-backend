d90fb51457c082e58b4ed1d99bdab7f99de64271
DavidRnR <carlosdavidmartin@gmail.com>
2024-09-15 16:18:23 -0300

    Railway config init

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize';
 import { DB_CONFIG } from './config';
-
+console.log(DB_CONFIG);
 const sequelize = new Sequelize(DB_CONFIG.NAME, DB_CONFIG.USERNAME, DB_CONFIG.PASSWORD, {
   host: DB_CONFIG.HOST,
   dialect: 'mysql',
