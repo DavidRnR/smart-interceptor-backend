2635010cefb9446f44beac3b812e00cacefff91f
DavidRnR <carlosdavidmartin@gmail.com>
2024-09-15 19:17:05 -0300

    Railway config (#2)
    * Railway config init
    
    * Debugging railway deploy
    
    * Ignore db connection at the begin
    
    * App port config
    
    * Default values JWT
    
    * Db table names
    
    * Scripts db names

--- db/setupDB.ts
+++ db/setupDB.ts
@@ -1,43 +1,37 @@
-import fs from "fs";
-import path from "path";
-import sequelize from "../src/config/db";
+import fs from 'fs';
+import path from 'path';
+import sequelize from '../src/config/db';
 
 const runMigration = async (file: string) => {
-  const sql = fs.readFileSync(
-    path.join(__dirname, "scripts", "migrations", file),
-    "utf8"
-  );
+  const sql = fs.readFileSync(path.join(__dirname, 'scripts', 'migrations', file), 'utf8');
   await sequelize.query(sql);
 };
 
 const runSeeder = async (file: string) => {
-  const sql = fs.readFileSync(
-    path.join(__dirname, "scripts", "seeders", file),
-    "utf8"
-  );
+  const sql = fs.readFileSync(path.join(__dirname, 'scripts', 'seeders', file), 'utf8');
   await sequelize.query(sql);
 };
 
 const setupDb = async () => {
   try {
     await sequelize.authenticate();
-    console.log("Connected to the database.");
+    console.log('Connected to the database.');
 
     // Run migrations
-    await runMigration("001-create-users-table.sql");
-    await runMigration('002-create-os-table.sql'); 
-    await runMigration('003-create-pl-table.sql'); 
-    console.log("Migration complete.");
+    await runMigration('001-create-user-table.sql');
+    await runMigration('002-create-os-table.sql');
+    await runMigration('003-create-pl-table.sql');
+    console.log('Migration complete.');
 
     // Run seeders
-    await runSeeder("001-insert-users.sql");
-    await runSeeder("002-insert-os.sql");
-    await runSeeder("003-insert-pl.sql");
-    console.log("Seeding complete.");
+    await runSeeder('001-insert-user.sql');
+    await runSeeder('002-insert-os.sql');
+    await runSeeder('003-insert-pl.sql');
+    console.log('Seeding complete.');
 
     process.exit(0);
   } catch (error) {
-    console.error("Failed to set up the database:", error);
+    console.error('Failed to set up the database:', error);
     process.exit(1);
   }
 };
--- src/config/config.ts
+++ src/config/config.ts
@@ -4,14 +4,14 @@ dotenv.config({
 });
 
 export const JWT_CONFIG = {
-  JWT_ACCESS_TOKEN_SECRET: process.env.JWT_ACCESS_TOKEN_SECRET || 'secret',
-  JWT_REFRESH_TOKEN_SECRET: process.env.JWT_REFRESH_TOKEN_SECRET || 'secret',
+  JWT_ACCESS_TOKEN_SECRET: process.env.JWT_ACCESS_TOKEN_SECRET || 'testNodeJS',
+  JWT_REFRESH_TOKEN_SECRET: process.env.JWT_REFRESH_TOKEN_SECRET || 'refreshNodeJS',
   JWT_ACCESS_TOKEN_LIFE: Number(process.env.JWT_ACCESS_TOKEN_LIFE) || 900,
   JWT_REFRESH_TOKEN_LIFE: Number(process.env.JWT_REFRESH_TOKEN_LIFE) || 86400,
 };
 
 export const SERVER_CONFIG = {
-  PORT: Number(process.env.APP_PORT) || 5000,
+  PORT: Number(process.env.PORT) || 5000,
 };
 
 export const DB_CONFIG = {
