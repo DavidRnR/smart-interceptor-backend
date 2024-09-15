adaba26f472961c80681b523b52750900d5cd19f
DavidRnR <carlosdavidmartin@gmail.com>
2024-09-15 17:49:47 -0300

    Default values JWT

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,8 +4,8 @@ dotenv.config({
 });
 
 export const JWT_CONFIG = {
-  JWT_ACCESS_TOKEN_SECRET: process.env.JWT_ACCESS_TOKEN_SECRET || 'secret',
-  JWT_REFRESH_TOKEN_SECRET: process.env.JWT_REFRESH_TOKEN_SECRET || 'secret',
+  JWT_ACCESS_TOKEN_SECRET: process.env.JWT_ACCESS_TOKEN_SECRET || 'testNodeJS',
+  JWT_REFRESH_TOKEN_SECRET: process.env.JWT_REFRESH_TOKEN_SECRET || 'refreshNodeJS',
   JWT_ACCESS_TOKEN_LIFE: Number(process.env.JWT_ACCESS_TOKEN_LIFE) || 900,
   JWT_REFRESH_TOKEN_LIFE: Number(process.env.JWT_REFRESH_TOKEN_LIFE) || 86400,
 };
@@ -13,8 +13,6 @@ export const JWT_CONFIG = {
 export const SERVER_CONFIG = {
   PORT: Number(process.env.PORT) || 5000,
 };
-
-console.log(SERVER_CONFIG);
 
 export const DB_CONFIG = {
   NAME: process.env.DB_NAME || 'dbname',
